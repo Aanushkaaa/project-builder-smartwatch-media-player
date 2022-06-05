4d95e1545267c889459a343c47b366a8ff7a37f0
Anushka Pathak <91602302+Aanushkaaa@users.noreply.github.com>
2022-06-05 14:35:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,20 +8,20 @@ setInterval(() => {
     document.getElementById("time").innerHTML=hour+":"+min;
     document.getElementById("day").innerHTML=daylist[day];
     },1000);
-
+   
   let msgList = [
       "Java is a high-level, class-based, object-oriented programming language that is designed to have as few implementation dependencies as possible.",
       "Cascading Style Sheets is a style sheet language used for describing the presentation of a document written in a markup language such as HTML.",
       "The HyperText Markup Language, or HTML is the standard markup language for documents designed to be displayed in a web browser.",
       "JavaScript, often abbreviated as JS, is a programming language that conforms to the ECMAScript specification.",
     ];
-
+    
     let selectMsg = (msgId) => {
       let msg = document.getElementById("showMsg");
       msg.style.display = "block";
       msg.textContent = msgList[msgId - 1];
     };
-
+  
     //Media
     let smsState, musicState, watchState;
     smsState = musicState = watchState = false;
@@ -34,7 +34,7 @@ setInterval(() => {
       document.querySelector(".sms").style.display = "none";
       document.querySelector(".media").style.display = "none";
   }
-
+  
     function selectScreen(s) {
       setOff();
       let icons = document.querySelector(".icons").childNodes;
